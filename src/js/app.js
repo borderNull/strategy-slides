5cd1a8790c10b0e93977762416c7c9cb5a7f94d1
borderNull <spbtvtower@gmail.com>
2017-02-02 22:49:25 +0300

    (css) fix: max screen fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 
 document.addEventListener ('DOMContentLoaded', function () {
-    logoslider()
-    menuIcon.addEventListener('click', function() {
-        this.parentNode.classList.toggle('active');
-        this.classList.toggle('active');
 
-    })
+
+    // let brownShutter = document.querySelector('.profySlideBrownImg');
+    
+    // brownShutter.style.left = 0;
+    // logoslider()
+    // menuIcon.addEventListener('click', function() {
+    //     this.parentNode.classList.toggle('active');
+    //     this.classList.toggle('active');
+
+    // })
 })
